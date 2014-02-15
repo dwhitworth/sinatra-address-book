aad4dcf98d83ed9662fd9277f96238892a5843d5
Donovan Whitworth <donovan.whitworth@gmail.com>
2014-02-14 21:44:51 -0600

    add photos page and photo upload capability

--- address_book.rb
+++ address_book.rb
@@ -5,4 +5,20 @@ class AddressBook < Sinatra::Base
   get '/' do
     slim :home
   end
+
+  get '/photos' do
+    slim :photos
+  end
+
+  post '/photos' do
+    if params[:photo].nil?
+      redirect '/photos'
+      return
+    end
+    filename = File.join(settings.root, 'files', params[:photo][:filename])
+    File.open(filename, 'w') do |file|
+      file.write params[:photo][:tempfile].read
+    end
+    "OK, photo saved"
+  end
 end
